85cce3af991c59b693ffb751e0450b09977e5577
pwdcat <113306139+pwdcat@users.noreply.github.com>
2024-01-02 15:55:38 -0500

    Inital startup, add html and js files Added getComputerChoice() function in js file     - Gets random choice betweeen 'Rock', 'Paper', and 'Scissors'

---
+++ main.js
@@ -0,0 +1,11 @@
+function getComputerChoice(){
+    let num = Math.floor(Math.random() * 3)
+    switch(num){
+        case 0:
+            return 'Rock'
+        case 1:
+            return 'Paper'
+        case 2:
+            return 'Scissors'
+    }
+}

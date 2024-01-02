40d12ead5482f2641e2aec2c56d4202720162444
pwdcat <113306139+pwdcat@users.noreply.github.com>
2024-01-02 16:13:47 -0500

    Add playRound() function Plays a round with the computer and returns a string to tell whether the player won or not

--- main.js
+++ main.js
@@ -9,3 +9,32 @@ function getComputerChoice(){
             return 'Scissors'
     }
 }
+
+function playRound(playerSelection, computerSection){
+    if(playerSelection.toUpperCase() == computerSection.toUpperCase())
+        return "You Tied!"
+
+    switch(playerSelection.toUpperCase()){
+        case "ROCK":
+            if(computerSection == 'Paper'){
+                return "You Lose! Paper beats Rock"
+            }
+            else{
+                return "You Won! Rock beats Scissors"
+            }
+        case "PAPER":
+            if(computerSection == 'Scissors'){
+                return "You Lose! Scissors beats Paper"
+            }
+            else{
+                return "You Won! Paper beats Rock"
+            }
+        case "SCISSORS":
+            if(computerSection == 'Rock'){
+                return "You Lose! Rock beats Scissors"
+            }
+            else{
+                return "You Won! Scissors beats Paper"
+            }
+    }
+}

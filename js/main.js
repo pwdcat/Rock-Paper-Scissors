b08a132f6b4c466e8eb54f4527ef65869f017283
pwdcat <113306139+pwdcat@users.noreply.github.com>
2024-01-03 13:05:57 -0500

    Basic score system, remove console.log() Made the website more interactable Outputs if you tied, lost, or won

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+let playerScore = 0;
+let computerScore = 0;
+
+const buttons = document.querySelectorAll('button');
+const results = document.getElementById('results');
+const computer = document.getElementById('computer');
+const player = document.getElementById('player');
+
+buttons.forEach((button) =>{
+    button.addEventListener('click', ()=> {
+        results.textContent = playRound(button.id, getComputerChoice());
+    });
+});
+
 function getComputerChoice(){
     let num = Math.floor(Math.random() * 3)
     switch(num){
@@ -13,37 +27,45 @@ function getComputerChoice(){
 function playRound(playerSelection, computerSection){
     if(playerSelection.toUpperCase() == computerSection.toUpperCase())
         return "You Tied!"
-
+    let output = '';
     switch(playerSelection.toUpperCase()){
         case "ROCK":
             if(computerSection == 'Paper'){
-                return "You Lose! Paper beats Rock"
+                computerScore++;
+                output = "You Lose! Paper beats Rock"
             }
             else{
-                return "You Won! Rock beats Scissors"
+                playerScore++;
+                output = "You Won! Rock beats Scissors"
             }
+            break;
         case "PAPER":
             if(computerSection == 'Scissors'){
-                return "You Lose! Scissors beats Paper"
+                computerScore++;
+                output = "You Lose! Scissors beats Paper"
             }
             else{
-                return "You Won! Paper beats Rock"
+                playerScore++;
+                output = "You Won! Paper beats Rock"
             }
+            break;
         case "SCISSORS":
             if(computerSection == 'Rock'){
-                return "You Lose! Rock beats Scissors"
+                computerScore++;
+                output = "You Lose! Rock beats Scissors"
             }
             else{
-                return "You Won! Scissors beats Paper"
+                playerScore++;
+                output = "You Won! Scissors beats Paper"
             }
+            break;
     }
+
+    computer.textContent = computerScore;
+    player.textContent = playerScore;
+    return output;
+
 }
 
-const buttons = document.querySelectorAll('button');
 
-buttons.forEach((button) =>{
-    button.addEventListener('click', ()=> {
-        console.log(playRound(button.id, getComputerChoice()));
-    });
-});
 
